8189c695b14e677c56cf6af2e418582bcf0f8ff7
tjudoubi <2624593014@qq.com>
2018-09-04 17:50:15 +0800

    a caculator

---
+++ Struct.java
@@ -0,0 +1,13 @@
+package pojectwork;
+
+public class Struct {
+	public int flag;
+	public Float num;
+	public String sign;
+	public Struct(int i, Float valueOf, String ob){
+		flag = i;
+		num = valueOf;
+		sign = ob;
+		// TODO Auto-generated constructor stub
+	}
+}
